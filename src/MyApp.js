52613787b5fabcd197ad5cc798106ff3ea02fc27
hkrhasan <hkrhasan8010@gmail.com>
2023-02-18 21:07:59 +0530

    state and lifecycle methods in class component

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,25 +1,88 @@
-import "./App.css";
-import Footer from "./components/Footer";
-import Main from "./components/Main";
-import Header from "./components/Header";
-import { Component } from "react";
+import React, { Component } from "react";
 
-function App() {
-  const isLoggedIn = true;
+export default class MyApp extends Component {
+  // constructor => 1
+  constructor() {
+    super();
+    this.state = {
+      showChild: true,
+    };
 
-  if (isLoggedIn) {
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  // render => 2
+  render() {
     return (
-      <div className="App">
-        <Header />
-        <Main>
-          <div>this child component</div>
-        </Main>
-        <Footer />
+      <div>
+        <div>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={this.onChange}
+          />
+          <input
+            type="text"
+            name="age"
+            placeholder="Age"
+            onChange={this.onChange}
+          />
+          <input
+            type="text"
+            name="contact"
+            placeholder="Contact"
+            onChange={this.onChange}
+          />
+        </div>
+        {this.state.showChild ? <UserView user={this.state} /> : null}
+
+        <button
+          onClick={(e) => {
+            this.setState({ showChild: !this.state.showChild });
+          }}
+        >
+          {this.state.showChild ? "Hide" : "Show"}
+        </button>
+      </div>
+    );
+  }
+}
+
+class UserView extends Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+  }
+
+  render() {
+    console.log("render");
+    const { name, age, contact } = this.props.user;
+    return (
+      <div>
+        <h1>Name: {name}</h1>
+        <h1>Age: {age}</h1>
+        <h1>Contact: {contact}</h1>
       </div>
     );
   }
 
-  return <div>I think you're un-authorize user</div>;
-}
+  componentDidMount() {
+    const h1 = document.querySelector("h1");
+    console.log(h1);
+  }
 
-export default App;
+  componentWillUnmount() {
+    const h1 = document.querySelector("h1");
+    console.log(h1);
+
+    console.log("componentWillUnmount");
+  }
+}
